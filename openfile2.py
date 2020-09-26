c79bd8e7b9d21456db00dcf550a8812cb16be732
pompamiron <71726498+pompamiron@users.noreply.github.com>
2020-09-27 02:43:44 +0700

    Create openfile2.py

---
+++ openfile2.py
@@ -0,0 +1,24 @@
+# Chalakorn Manopirom // Open file, find the target value and find its average
+
+# Write a program that prompts for a file name, then opens that file and reads through the file, looking for lines of the form:
+#         X-DSPAM-Confidence:    0.8475
+# Count these lines and extract the floating point values from each of the lines and compute the average of those values and produce an output as shown below. 
+# Do not use the sum() function or a variable named sum in your solution.
+# You can download the sample data at http://www.py4e.com/code3/mbox-short.txt
+
+# Use the file name mbox-short.txt as the file name
+fname = input("Enter file name: ")
+fh = open(fname)
+count = 0
+value = 0
+for line in fh:
+    if line.startswith("X-DSPAM-Confidence:") : 
+        lookup = line.find(':')
+        use = line[lookup+1:]
+        use1 = use.strip()
+        number = float(use1)
+        value = value + number
+        count = count+1
+    else:
+        continue
+print("Average spam confidence:",value/count)
